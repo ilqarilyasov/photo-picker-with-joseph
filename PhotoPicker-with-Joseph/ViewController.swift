55ba2e5d412abecba0c979aa29df0d583c4f4371
Ilqar Ilyasov <imilqar@gmail.com>
2018-09-12 10:27:54 -0500

    Initial commit.

---
+++ PhotoPicker-with-Joseph/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  PhotoPicker-with-Joseph
+//
+//  Created by Ilgar Ilyasov on 9/12/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+
+}
+
